240a564b30b34877128603b605009d2c0babc28b
Rye <rye@localhost.localdomain>
2015-10-28 16:15:43 -0700

    add all files

---
+++ static/javascript/hamburgler.js
@@ -0,0 +1,32 @@
+// HAMBURGLERv2
+
+function togglescroll () {
+  $('html').on('touchstart', function(e){
+    if ($('html').hasClass('noscroll')) {
+      e.preventDefault();
+    }
+  });
+}
+
+$(document).ready(function () {
+    togglescroll()
+    $(".icon").click(function () {
+        $(".mobilenav").fadeToggle(300);
+        $(".top-menu").toggleClass("top-animate");
+        $("html").toggleClass("noscroll");
+        $(".mid-menu").toggleClass("mid-animate");
+        $(".bottom-menu").toggleClass("bottom-animate");
+    });
+});
+
+// PUSH ESC KEY TO EXIT
+
+$(document).keydown(function(e) {
+    if (e.keyCode == 27) {
+        $(".mobilenav").fadeOut(300);
+        $(".top-menu").removeClass("top-animate");
+        $("html").removeClass("noscroll");
+        $(".mid-menu").removeClass("mid-animate");
+        $(".bottom-menu").removeClass("bottom-animate");
+    }
+});
